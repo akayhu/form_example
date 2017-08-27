6eee83270cb302aef8edb2da78b04d0a6aed75ae
Akay Hu <god75615@gmail.com>
2017-08-27 17:04:17 +0800

    防呆判斷

--- js/all.js
+++ js/all.js
@@ -4,7 +4,12 @@ $(function(){
 
   formObj = ( function($) {
 
-    $constructor = {};
+    $constructor = {
+      username: false,
+      email: false,
+      password: false,
+      re_password: false
+    };
 
     $inputName = {
       username: $("#username"),
@@ -13,7 +18,7 @@ $(function(){
       re_password: $("#re_password")
     };
 
-    $error_message = {
+    $errorDiv = {
       username: $("#error_message_username"),
       email: $("#error_message_email"),
       password: $("#error_message_password"),
@@ -24,10 +29,83 @@ $(function(){
       submit: $(".submit")
     };
 
+    $errorMessage = {
+      error_username: "✕ 請輸入姓名",
+      error_email1: "✕ 請輸入Email",
+      error_email2: "✕ Email格式錯誤",
+      error_password: "✕ 請輸入密碼",
+      error_re_password1: "✕ 請輸入再次確認密碼",
+      error_re_password2: "✕ 與密碼不一致，請重新輸入"
+    };
+
+    submit = function(){
+      var data = {
+        username: $inputName.username.val(),
+        email: $inputName.email.val(),
+        password: $inputName.password.val()
+      };
+      console.log( '送出', data );
+    },
+
+    showError = function( showTag, showMessage ){
+      showTag.html( showMessage );
+    },
+
+    hideError = function( hideTag ){
+      hideTag.html('');
+    };
+
     return {
 
       init: function(){
-        console.log("555");
+
+        $button.submit.click( function() {
+
+          var reg = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+          // username
+          if( !$inputName.username.val() ){
+            showError( $errorDiv.username, $errorMessage.error_username );
+          } else {
+            hideError( $errorDiv.username );
+            $constructor.username = true;
+          }
+          
+          // email
+          if( !$inputName.email.val() ) {
+            showError( $errorDiv.email, $errorMessage.error_email1 );
+          } else if( !reg.test( $inputName.email.val() ) ) {
+            showError( $errorDiv.email, $errorMessage.error_email2 );
+          } else {
+            hideError( $errorDiv.email );
+            $constructor.email = true;
+          }
+
+          // password
+          if( !$inputName.password.val() ){
+            showError( $errorDiv.password, $errorMessage.error_password );
+          } else {
+            hideError( $errorDiv.password );
+            $constructor.password = true;
+          }
+
+          // re_password
+          if( !$inputName.re_password.val() ) {
+            showError( $errorDiv.re_password, $errorMessage.error_re_password1 );
+          } else if( $inputName.password.val() !== $inputName.re_password.val() ) {
+            showError( $errorDiv.re_password, $errorMessage.error_re_password2 );
+          } else {
+            hideError( $errorDiv.re_password );
+            $constructor.re_password = true;
+          }
+
+          console.log( '$constructor', $constructor );
+          if( $constructor.username && $constructor.email &&  $constructor.password && $constructor.re_password ) {
+            submit();
+          }
+
+        });
+
       }
 
     }
